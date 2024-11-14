9b14f39e3fbe4e2089d987a1b0f9878057623001
Ladydee3 <delisiletwala62@gmail.com>
2024-11-14 20:25:57 +0000

    changes

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,42 +1,55 @@
 #!/usr/bin/env python3
-"""
-Manage API authentication system
-"""
+""" Auth module """
 from flask import request
 from typing import List, TypeVar
+from models.user import User
+from os import getenv
 
 
-class Auth():
-    """
-    Manage API authentication methods
-    """
+class Auth:
+    """ class auth for authenticating users"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Return boolean """
-        if path is None or excluded_paths is None or not len(excluded_paths):
+        """ require auth function that returns false"""
+        if excluded_paths and path:
+            if path[-1] == '/':
+                new_path = path[:-1]
+            else:
+                new_path = path
+            new_excluded_path = []
+            for element in excluded_paths:
+                if element[-1] == '/':
+                    new_excluded_path.append(element[:-1])
+                if element[-1] == '*':
+                    if new_path.startswith(element[:-1]):
+                        return False
+
+            if new_path not in new_excluded_path:
+                return True
+            else:
+                return False
+        if path is None:
+            return True
+        if not excluded_paths:
             return True
 
-        if path[-1] != '/':
-            path += '/'
-        if excluded_paths[-1] != '/':
-            excluded_paths += '/'
-
-        astericks = [stars[:-1]
-                     for stars in excluded_paths if stars[-1] == '*']
-
-        for stars in astericks:
-            if path.startswith(stars):
-                return False
-
-        if path in excluded_paths:
-            return False
-        return True
-
     def authorization_header(self, request=None) -> str:
-        """ Request Flask object """
-        if request is None or 'Authorization' not in request.headers:
+        """ authorization header"""
+        if request is None:
             return None
-        return request.headers.get('Authorization')
+        authorization = request.headers.get('Authorization')
+        if authorization is None:
+            return None
+        else:
+            return authorization
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Flask request object """
+        """ return current user else None"""
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns request cookie value """
+        if request is None:
+            return None
+        cookie = getenv('SESSION_NAME')
+        return request.cookies.get(cookie)
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,61 +1,42 @@
 #!/usr/bin/env python3
-""" Module of Session Auth
-"""
+"""Handles all routes for the Session Authentication."""
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import request, jsonify, make_response, abort
 from models.user import User
 from os import getenv
 
 
-@app_views.route('/auth_session/logout',
-                 methods=['DELETE'],
-                 strict_slashes=False)
-def session_logout() -> str:
-    """ DELETE /api/v1/auth_session/logout
-    JSON body:
-    - session id
-    Return:
-      - Empty JSON
-    """
-    from api.v1.app import auth
-
-    logout = auth.destroy_session(request)
-    if not logout:
-        abort(404)
-    return jsonify({}), 200
-
-
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_login() -> str:
-    """ POST /api/v1/auth_session/login
-    JSON body:
-      - email
-      - password
-    Return:
-      - User object JSON represented
-    """
-
-    user_email = request.form.get('email')
-    user_pswd = request.form.get('password')
-
-    if not user_email:
+def session() -> str:
+    """Flask view for Session authentication."""
+    email = request.form.get('email')
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if not user_pswd:
+
+    password = request.form.get('password')
+    if not password:
         return jsonify({"error": "password missing"}), 400
 
-    try:
-        search_users = User.search({'email': user_email})
-    except Exception:
-        return jsonify({"error": "no user found for this email"}), 404
-    if not search_users:
+    user = User.search({'email': email})
+    if not user:
         return jsonify({"error": "no user found for this email"}), 404
 
-    user = search_users[0]
-    if not user.is_valid_password(user_pswd):
+    obj = user[0]
+    if not obj.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
+
     from api.v1.app import auth
-    session_cookie = getenv("SESSION_NAME")
-    session_id = auth.create_session(user.id)
-    response = jsonify(user.to_json())
-    response.set_cookie(session_cookie, session_id)
+    session_id = auth.create_session(obj.id)
+    response = make_response(obj.to_json())
+    response.set_cookie(getenv('SESSION_NAME'), session_id)
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_session() -> str:
+    """View to deleting session/ logging out."""
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
